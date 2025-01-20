016fdd35088826a2649f0fb6413c8c3b9a338aa0
Nexito <soporte@nexito.tech>
2025-01-20 11:59:22 +0100

    cambiado a static deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,10 @@ import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import compress from 'astro-compress';
 import robotsTxt from 'astro-robots-txt';
-import vercel from '@astrojs/vercel/serverless'; // Cambiado a serverless
+import vercel from '@astrojs/vercel/static'; // Cambiado a static
 
 export default defineConfig({
-  output: 'server',
+  output: 'static',  // Cambiado a static
   adapter: vercel(),
   site: 'https://nexito.tech',
   integrations: [
