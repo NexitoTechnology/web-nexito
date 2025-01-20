2ccc6e612edc6524749091e792a24e8e24ff8ca1
Nexito <soporte@nexito.tech>
2025-01-20 11:46:12 +0100

    configurado adaptador vercel static

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import compress from 'astro-compress';
 import robotsTxt from 'astro-robots-txt';
+import vercel from '@astrojs/vercel/static';
 
 export default defineConfig({
   output: 'server',
