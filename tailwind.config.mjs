95a104e5d90d7c4f8644bed69b66dbfdf33bcbe0
Adrián <admin@nexito.tech>
2025-01-21 23:40:02 +0100

    Dividir por Componentes las paginas de Servicios

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,9 +2,32 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-	  extend: {},
+	  extend: {
+		keyframes: {
+		  blob: {
+			'0%': {
+			  transform: 'translate(0px, 0px) scale(1)',
+			},
+			'33%': {
+			  transform: 'translate(30px, -50px) scale(1.1)',
+			},
+			'66%': {
+			  transform: 'translate(-20px, 20px) scale(0.9)',
+			},
+			'100%': {
+			  transform: 'translate(0px, 0px) scale(1)',
+			},
+		  }
+		},
+		animation: {
+		  blob: 'blob 7s infinite',
+		},
+		backgroundImage: {
+		  'grid-white': 'url("/grid.svg")', // Asegúrate de tener este recurso
+		}
+	  },
 	},
 	plugins: [
-	  require('@tailwindcss/typography'), // Asegúrate de que esto está incluido
+	  require('@tailwindcss/typography'), // Plugin de typography
 	],
   }
