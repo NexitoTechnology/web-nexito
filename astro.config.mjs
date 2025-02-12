349faa50dc7ed44b0089038e71408d8740d4b835
Tu Nombre <admin@nexito.tech>
2025-02-12 08:30:01 +0100

    Correción Redireccionar 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,143 +9,141 @@ export default defineConfig({
   redirects: {
     '/servicios': {
       destination: '/',
-      permanent: true // Esto configura un 301 (redirección permanente)
+      status: 301
     },
-    '/servicios': {
+    '/servicios/': {
       destination: '/',
-      permanent: true // Esto configura un 301 (redirección permanente)
+      status: 301
     }
   },
- viewTransitions: true,
- output: 'static',
- site: 'https://nexito.tech',
- base: '/',
- integrations: [
-  partytown({
-    config: {
-      forward: ["dataLayer.push"],
+  headers: {
+    'Cache-Control': 'public, max-age=31536000, immutable',
+    'Content-Security-Policy': "default-src 'self' 'unsafe-inline' 'unsafe-eval' https:; img-src 'self' data: https:; style-src 'self' 'unsafe-inline' https:; connect-src 'self' https:; script-src 'self' 'unsafe-inline' 'unsafe-eval' https: https://www.googletagmanager.com;",
+    'X-Frame-Options': 'SAMEORIGIN',
+    'Referrer-Policy': 'strict-origin-when-cross-origin',
+    'X-Content-Type-Options': 'nosniff'
+  },
+  viewTransitions: true,
+  output: 'static',
+  site: 'https://nexito.tech',
+  base: '/',
+  integrations: [
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+    tailwind(),
+    sitemap({
+      changefreq: 'weekly',
+      priority: 0.7,
+      lastmod: new Date(),
+      i18n: {
+        defaultLocale: 'es',
+        locales: {
+          es: 'es-ES',
+          en: 'en-US',
+        },
+      },
+      filter: (page) => ![
+        'aviso-legal',
+        'politica-de-cookies',
+        'politica-de-privacidad',
+      ].some((path) => page.includes(path)),
+    }),
+    compress({
+      CSS: true,
+      HTML: true,
+      JavaScript: true,
+      SVG: true,
+      Logger: 1,
+      minify: true,
+      gzip: true,
+      brotli: true,
+      inline: {
+        css: true
+      },
+      compress: {
+        threshold: 0,
+      },
+    }),
+    robotsTxt(),
+  ],
+  image: {
+    service: {
+      entrypoint: 'astro/assets/services/sharp',
+      config: {
+        quality: 80,
+        formats: ['webp', 'avif'],
+        svg: {
+          optimization: true,
+          cleanupAttrs: true,
+          removeDoctype: true,
+          removeXMLProcInst: true,
+          removeComments: true,
+          removeMetadata: true,
+          removeTitle: true,
+          removeDesc: true,
+          removeUselessDefs: true,
+          removeEditorsNSData: true,
+          removeEmptyAttrs: true,
+          removeEmptyContainers: true,
+          minifyStyles: true,
+          convertStyleToAttrs: true,
+          convertColors: true,
+          convertPathData: true,
+          convertTransform: true,
+          removeUnknownsAndDefaults: true,
+          removeNonInheritableGroupAttrs: true,
+          removeUselessStrokeAndFill: true,
+          removeUnusedNS: true,
+          cleanupIDs: true,
+          cleanupNumericValues: true,
+          cleanupListOfValues: true,
+          moveGroupAttrsToElems: true,
+          collapseGroups: true,
+          removeRasterImages: false,
+          mergePaths: true,
+          convertShapeToPath: true,
+          sortAttrs: true,
+          removeDimensions: true,
+        }
+      },
     },
-  }),
-   tailwind(),
-   sitemap({
-     changefreq: 'weekly',
-     priority: 0.7,
-     lastmod: new Date(),
-     i18n: {
-       defaultLocale: 'es',
-       locales: {
-         es: 'es-ES',
-         en: 'en-US',
-       },
-     },
-     filter: (page) => ![
-       'aviso-legal',
-       'politica-de-cookies',
-       'politica-de-privacidad',
-     ].some((path) => page.includes(path)),
-   }),
-   compress({
-     CSS: true,
-     HTML: true,
-     JavaScript: true,
-     SVG: true,
-     Logger: 1,
-     minify: true,
-     gzip: true,
-     brotli: true,
-     inline: {
-       css: true
-     },
-     compress: {
-       threshold: 0,
-     },
-   }),
-   robotsTxt(),
- ],
- image: {
-   service: {
-     entrypoint: 'astro/assets/services/sharp',
-     config: {
-       quality: 80,
-       formats: ['webp', 'avif'],
-       svg: {
-         optimization: true,
-         cleanupAttrs: true,
-         removeDoctype: true,
-         removeXMLProcInst: true,
-         removeComments: true,
-         removeMetadata: true,
-         removeTitle: true,
-         removeDesc: true,
-         removeUselessDefs: true,
-         removeEditorsNSData: true,
-         removeEmptyAttrs: true,
-         removeEmptyContainers: true,
-         minifyStyles: true,
-         convertStyleToAttrs: true,
-         convertColors: true,
-         convertPathData: true,
-         convertTransform: true,
-         removeUnknownsAndDefaults: true,
-         removeNonInheritableGroupAttrs: true,
-         removeUselessStrokeAndFill: true,
-         removeUnusedNS: true,
-         cleanupIDs: true,
-         cleanupNumericValues: true,
-         cleanupListOfValues: true,
-         moveGroupAttrsToElems: true,
-         collapseGroups: true,
-         removeRasterImages: false,
-         mergePaths: true,
-         convertShapeToPath: true,
-         sortAttrs: true,
-         removeDimensions: true,
-       }
-     },
-   },
- },
- vite: {
-   optimizeDeps: {
-     entries: [], // Forzar precompilación de dependencias
-   },
-   css: {
-     devSourcemap: true,
-   },
-   server: {
-     headers: {
-      'Cache-Control': 'public, max-age=31536000, immutable',
-      'Content-Security-Policy': "default-src 'self';", // Política de seguridad de contenido
-      'X-Frame-Options': 'SAMEORIGIN', // Previene clickjackin
-      'Referrer-Policy': 'strict-origin-when-cross-origin', // Controla cómo el referrer debe ser enviado
-      'X-Content-Type-Options': 'nosniff' // Previene que el navegador MIME-sniff la respuesta
-     }
-   },
-   build: {
-     cssCodeSplit: true,
-     minify: 'terser',
-     assetsInlineLimit: 4096,
-     rollupOptions: {
-       output: {
-         manualChunks(id) {
-           if (id.includes('node_modules')) {
-             return 'vendor';
-           }
-         },
-         assetFileNames: (assetInfo) => {
-           let extType = assetInfo.name.split('.').pop();
-           if (/css/.test(extType)) {
-             return `assets/css/[name]-[hash].${extType}`;
-           }
-           return `assets/[ext]/[name]-[hash].[ext]`;
-         },
-         entryFileNames: 'assets/js/[name]-[hash].js',
-         chunkFileNames: 'assets/js/[name]-[hash].js'
-       }
-     },
-     inlineDynamicImports: false,
-     assets: {
-       defaultCacheControl: 'public, max-age=31536000, immutable'
-     }
-   }
- }
+  },
+  vite: {
+    optimizeDeps: {
+      entries: [],
+    },
+    css: {
+      devSourcemap: true,
+    },
+    build: {
+      cssCodeSplit: true,
+      minify: 'terser',
+      assetsInlineLimit: 4096,
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return 'vendor';
+            }
+          },
+          assetFileNames: (assetInfo) => {
+            let extType = assetInfo.name.split('.').pop();
+            if (/css/.test(extType)) {
+              return `assets/css/[name]-[hash].${extType}`;
+            }
+            return `assets/[ext]/[name]-[hash].[ext]`;
+          },
+          entryFileNames: 'assets/js/[name]-[hash].js',
+          chunkFileNames: 'assets/js/[name]-[hash].js'
+        }
+      },
+      inlineDynamicImports: false,
+      assets: {
+        defaultCacheControl: 'public, max-age=31536000, immutable'
+      }
+    }
+  }
 });
