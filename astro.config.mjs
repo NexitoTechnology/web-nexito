0c60ee44cb012133eba4260c2f3644b3261184c0
Adrián Martín Toro <admin@nexito.tech>
2025-02-13 07:19:39 +0100

    Inicio Subida a Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,19 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import vercel from '@astrojs/vercel/serverless';
+
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({ 
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+      },
+      maxDuration: 8,
+    }),
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
