9fa83c0dd2666df018c6f3bf6dead6c334230bc3
Adrián Martín Toro <admin@nexito.tech>
2025-02-18 13:41:28 +0100

    API Event Confirm Call

---
+++ src/pages/api/confirm-shedule-call.ts
@@ -0,0 +1,13 @@
+// src/pages/api/track.ts
+import type { APIRoute } from 'astro';
+
+export const POST: APIRoute = async ({ request }) => {
+  const data = await request.json();
+  
+  window.dataLayer.push({
+    event: data.eventName,
+    ...data.eventData
+  });
+
+  return new Response(JSON.stringify({ success: true }));
+};
