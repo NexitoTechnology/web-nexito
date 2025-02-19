71b2573d9f1fd6d7fae0d3328c29e15e5c64df01
Adrián Martín Toro <admin@nexito.tech>
2025-02-19 12:04:47 +0100

    Corregir redirecciones 5

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  //trailingSlash: 'always',
+  trailingSlash: 'always',
   site: "https://nexito.tech",
   output: "server",
   adapter: vercel({
